704ba6ec2afa1e69dc8c214c6e31d4f34d8db2aa
ablaneels <neels.abla@hotmail.com>
2025-04-21 02:45:01 +0200

    modify carousel to change depending of the path

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,34 +1,39 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 import './Carousel.css'
 import BootstrapCarousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Carousel() {
+const Carousel = () => {
+const location = useLocation();
+
+const getImagesForRoute = (pathname) => {
+    switch (pathname) {
+    case "/venue":
+        return ["/pictures/image_venue.png", "/pictures/image_venue.png", "/pictures/image_venue.png"];
+    case "/dresscode":
+        return ["/pictures/image_dresscode.png", "/pictures/image_dresscode.png", "/pictures/image_dresscode.png"];
+    default:
+        return ["/pictures/IMG_4108 1.png", "/pictures/IMG_4108 1.png", "/pictures/IMG_4108 1.png"];
+    }
+};
+
+const images = getImagesForRoute(location.pathname);
+
     return (
         <BootstrapCarousel data-bs-theme="light" className='carrousel'>
-        <BootstrapCarousel.Item>
-            <img
-            className="d-block w-100"
-            src="/pictures/IMG_4108 1.png"
-            alt="First slide"
-            />
-        </BootstrapCarousel.Item>
-        <BootstrapCarousel.Item>
-            <img
-            className="d-block w-100"
-            src="/pictures/IMG_4108 1.png"
-            alt="Second slide"
-            />
-        </BootstrapCarousel.Item>
-        <BootstrapCarousel.Item>
-            <img
-            className="d-block w-100"
-            src="/pictures/IMG_4108 1.png"
-            alt="Third slide"
-            />
-        </BootstrapCarousel.Item>
+        {images.map((src, index) => (
+            <BootstrapCarousel.Item>
+                <img
+                key={index}
+                className="d-block w-100"
+                src={src}
+                alt={`slide-${index}`}
+                />
+            </BootstrapCarousel.Item>
+        ))}
         </BootstrapCarousel>
-      );  
+    );  
 }
 
 export default Carousel
