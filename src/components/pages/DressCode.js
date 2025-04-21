c59c4b1bd9f87cb87a2f93e0ff3daa2a9c5ab1ee
ablaneels <neels.abla@hotmail.com>
2025-04-21 02:44:03 +0200

    add dress code content

--- src/components/pages/DressCode.js
+++ src/components/pages/DressCode.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
+import './DressCode.css'
 
 function DressCode() {
+  const { t } = useTranslation();
   return (
-    <div>
-      Dress Code
+    <div className='dresscode'>
+      <div className='title'>{t('dressCode_title')}</div>
+      <hr className='line'/>
+      <div className='color_title'>
+        {t('dressCode_color_title')}
+        <div className='color'/>
+      </div>
+      <div className='style_title'>
+        {t('dressCode_style_title')}
+        <div className='style'>{t('dressCode_style')}</div>
+      </div>
+      <div className='forbidden_title'>
+        {t('dressCode_forbidden_title')}
+        <div className='forbidden'>{t('dressCode_forbidden')}</div>
+      </div>
     </div>
   )
 }
