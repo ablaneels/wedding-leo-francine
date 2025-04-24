e043f8dd0bb38bf47b62cb9b8f0e6ffdd5fcd4df
ablaneels <neels.abla@hotmail.com>
2025-04-24 23:51:44 +0200

    add french trad

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { Link, useLocation } from 'react-router-dom'
 import './NavBar.css';
 import Open from "../icons/menu-open.svg"
 import Close from "../icons/close-square-svgrepo-com.svg"
+import { useTranslation } from 'react-i18next';
 
 
 function NavBar() {
@@ -13,6 +14,8 @@ function NavBar() {
     
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const { t } = useTranslation();
     
     const showButton = () => {
         if (window.innerWidth <= 960)
@@ -32,27 +35,27 @@ function NavBar() {
                 <ul className={click ? 'menu active': 'menu'}>
                     <li className='navbar-item'>
                         <Link to="/" className={selectedLink === '/' ? 'selected-navbar-link' : 'navbar-link'} onClick={closeMobileMenu}>
-                            HOME
+                            {t('navbar_home')}
                         </Link>
                     </li>
                     <li className='navbar-item'>
                         <Link to="/rsvp" className={selectedLink === '/rsvp' ? 'selected-navbar-link' : 'navbar-link'} onClick={closeMobileMenu}>
-                            RSVP
+                            {t('navbar_rsvp')}
                         </Link>
                     </li>
                     <li className='navbar-item'>
                         <Link to="/venue" className={selectedLink === '/venue' ? 'selected-navbar-link' : 'navbar-link'} onClick={closeMobileMenu}>
-                            VENUE
+                            {t('navbar_venue')}
                         </Link>
                     </li>
                     <li className='navbar-item'>
                         <Link to="/dresscode" className={selectedLink === '/dresscode' ? 'selected-navbar-link' : 'navbar-link'} onClick={closeMobileMenu}>
-                            DRESS CODE
+                            {t('navbar_dressCode')}
                         </Link>
                     </li>
                     <li className='navbar-item'>
                         <Link to="/agenda" className={selectedLink === '/agenda' ? 'selected-navbar-link' : 'navbar-link'} onClick={closeMobileMenu}>
-                            AGENDA
+                            {t('navbar_agenda')}
                         </Link>
                     </li>
                 </ul>
