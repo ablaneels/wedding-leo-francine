873197fd80f68088dde8c1f44645d396af0f7411
ablaneels <neels.abla@hotmail.com>
2025-04-24 23:32:52 +0200

    replace old pictures with new ones + add css for mobile version

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,11 +10,11 @@ const location = useLocation();
 const getImagesForRoute = (pathname) => {
     switch (pathname) {
     case "/venue":
-        return ["/pictures/image_venue.png", "/pictures/image_venue.png", "/pictures/image_venue.png"];
+        return ["/pictures/Domaine de Millon/319707155_225718826470843_5517565324097533581_n.jpg", "/pictures/Domaine de Millon/Domaine_de_Millon_71.jpg", "/pictures/Domaine de Millon/Domaine_de_Millon_77-scaled (1).jpg", "/pictures/Domaine de Millon/ShootingMillon-0043-scaled (1).jpg"];
     case "/dresscode":
-        return ["/pictures/image_dresscode.png", "/pictures/image_dresscode.png", "/pictures/image_dresscode.png"];
+        return ["/pictures/Dress Code Page/beautiful-wedding-dress-with-plume-is-dressed-mannequin.jpg"];
     default:
-        return ["/pictures/IMG_4108 1.png", "/pictures/IMG_4108 1.png", "/pictures/IMG_4108 1.png"];
+        return ["/pictures/Francine and Leo/IMG_0378.jpeg", "/pictures/Francine and Leo/IMG_4108.jpeg", "/pictures/Francine and Leo/IMG_4337.jpeg", "/pictures/Francine and Leo/IMG_5759.jpeg", "/pictures/Francine and Leo/IMG_6040.jpeg"];
     }
 };
 
@@ -26,7 +26,7 @@ const images = getImagesForRoute(location.pathname);
             <BootstrapCarousel.Item>
                 <img
                 key={index}
-                className="d-block w-100"
+                className="carousel-image"
                 src={src}
                 alt={`slide-${index}`}
                 />
