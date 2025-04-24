a2a83c743cbd0f5679757e53d702a92c865e6185
ablaneels <neels.abla@hotmail.com>
2025-04-24 23:35:51 +0200

    add tab to venue + add mobile version to venue

--- src/components/pages/Venue.js
+++ src/components/pages/Venue.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Venue.css'
 import { useTranslation } from 'react-i18next';
 
 function Venue() {
     const { t } = useTranslation();
+
+      // State to manage which tab is active
+    const [activeTab, setActiveTab] = useState('about');
+
+    // Function to handle tab switching
+    const handleTabClick = (tab) => {
+      setActiveTab(tab);
+    };
+
   return (
     <div className='venue'>
       <div className='title'>
@@ -11,25 +20,60 @@ function Venue() {
         <div className='website'>{t('venue_website')}</div>
       </div>
       <div className='tabs'>
-        <div className='tab'>{t('venue_about')}</div>
+        <div
+          className={`tab ${activeTab === 'about' ? 'active' : ''}`}
+          onClick={() => handleTabClick('about')}
+        >
+          {t('venue_about')}
+        </div>
         <div className='tab'>|</div>
-        <div className='tab'>{t('venue_sleeping')}</div>
+        <div
+          className={`tab ${activeTab === 'sleeping' ? 'active' : ''}`}
+          onClick={() => handleTabClick('sleeping')}
+        >
+          {t('venue_sleeping')}
+        </div>
         <div className='tab'>|</div>
-        <div className='tab'>{t('venue_transportation')}</div>
+        <div
+          className={`tab ${activeTab === 'transportation' ? 'active' : ''}`}
+          onClick={() => handleTabClick('transportation')}
+        >
+          {t('venue_transportation')}
+        </div>
       </div>
       <hr className='line'/>
       <div>
-        <div className='subtab_title'>{t('venue_description_title')}</div>
-        <div className='subtab_text'>{t('venue_description_text')}</div>
-      </div>
-      <div>
-        <div className='subtab_title'>{t('venue_accommodation_title')}</div>
-        <div className='subtab_text'>{t('venue_accommodation_text')}</div>
-      </div>
-      <hr className='line'/>
-      <div>
-        <div className='subtab_title'>{t('venue_address_title')}</div>
-        <div className='subtab_text'>{t('venue_address_text')}</div>
+        {activeTab === 'about' && (
+          <>
+            <div>
+              <div className='subtab_title'>{t('venue_description_title')}</div>
+              <div className='subtab_text'>{t('venue_description_text')}</div>
+            </div>
+            <div>
+              <div className='subtab_title'>{t('venue_accommodation_title')}</div>
+              <div className='subtab_text'>{t('venue_accommodation_text')}</div>
+            </div>
+            <hr className='line'/>
+            <div>
+              <div className='subtab_title'>{t('venue_address_title')}</div>
+              <div className='subtab_text'>{t('venue_address_text')}</div>
+            </div>
+          </>
+        )}
+
+        {activeTab === 'sleeping' && (
+          <>
+            <div className='subtab_title'>sleeping</div>
+            <div className='subtab_text'>sleeping</div>
+          </>
+        )}
+
+        {activeTab === 'transportation' && (
+          <>
+            <div className='subtab_title'>transportation</div>
+            <div className='subtab_text'>transportation</div>
+          </>
+        )}
       </div>
     </div>
   )
